e059c5890bbade0fc27878e2f7b7503834076d15
faehnrichh <h.faehnrich@fida.de>
2023-06-30 21:04:15 +0200

    initialer Stand

---
+++ src/main/java/de/faehne/groovyinspringboot/GroovyinspringbootApplication.java
@@ -0,0 +1,13 @@
+package de.faehne.groovyinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyinspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyinspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/faehne/groovyinspringboot/GroovyinspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.faehne.groovyinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovyinspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
